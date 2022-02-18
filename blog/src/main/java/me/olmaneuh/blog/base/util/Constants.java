327418296f682e1a6982706afb6698526364c978
olmaneuh <olmaneuh@gmail.com>
2022-02-17 21:14:57 -0600

    Blog: Add a new Constants class

---
+++ blog/src/main/java/me/olmaneuh/blog/base/util/Constants.java
@@ -0,0 +1,10 @@
+package me.olmaneuh.blog.base.util;
+
+public class Constants {
+
+    public static final String TABLE_NAME_USER = "tUser";
+    public  static final String TABLE_NAME_POST = "tPost";
+
+    private Constants() { }
+
+}
