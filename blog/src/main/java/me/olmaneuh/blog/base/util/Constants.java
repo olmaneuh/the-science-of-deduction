091077aca7adcd828dacb255b49f7b05b4a881f0
olmaneuh <olmaneuh@gmail.com>
2022-02-22 21:44:15 -0600

    Blog: Add new constants to the file

--- blog/src/main/java/me/olmaneuh/blog/base/util/Constants.java
+++ blog/src/main/java/me/olmaneuh/blog/base/util/Constants.java
@@ -5,6 +5,13 @@ public class Constants {
     public static final String TABLE_NAME_USER = "tUser";
     public  static final String TABLE_NAME_POST = "tPost";
 
+    public static final String TABLE_COLUMN_NAME_CREATED_DATE = "created_date";
+    public static final String TABLE_COLUMN_NAME_UPDATED_DATE = "updated_date";
+    public static final String TABLE_COLUMN_NAME_FIRST_NAME = "first_name";
+    public static final String TABLE_COLUMN_NAME_LAST_NAME = "last_name";
+
+    public static final String TABLE_COLUMN_FK_USERS_POSTS = "FK_USERS_POSTS";
+
     private Constants() { }
 
 }
