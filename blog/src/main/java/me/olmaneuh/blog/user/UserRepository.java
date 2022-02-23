a95a7b76a10d5bb9314c5c25acc98e4e2b364318
olmaneuh <olmaneuh@gmail.com>
2022-02-22 21:30:33 -0600

    Blog: Create User repository and service classes

---
+++ blog/src/main/java/me/olmaneuh/blog/user/UserRepository.java
@@ -0,0 +1,9 @@
+package me.olmaneuh.blog.user;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends PagingAndSortingRepository<User, Long> {
+
+}
